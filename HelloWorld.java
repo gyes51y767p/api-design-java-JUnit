beb80114b4fd914162dbbb62d0cd3b50d08d673e
Sheng Hao Liang <gyes51y767p@gmail.com>
2023-10-03 21:38:54 -0700

    add user input feature and edit the output text

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,21 +1,27 @@
 // HelloWorld.java entry point , class will be in a seperate file
 package api;
+import java.util.Scanner;
+
 
 public class HelloWorld {
-//     public void printHello() {
-//         System.out.println("Hello, World!");
-//     }
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a target string: ");
+        String userInput = scanner.nextLine();
         // Create a UserInputRegex object with user-provided string (treated as a literal string)
-        UserInputRegex userInputRegex = new UserInputRegex("hello");
+        UserInputRegex userInputRegex = new UserInputRegex(userInput);
+        
+        System.out.print("Enter a text: ");
+        String inputText = scanner.nextLine();
+        scanner.close();
 
         // Check if the pattern matches a user-provided string
-        String userString = "hello, world!";
-        if (userInputRegex.matchesBeginning(userString)) {
-            System.out.println("The user-defined pattern matches the beginning of the user string.");
+        //String userString = "hello, world!";
+        if (userInputRegex.matches(inputText)) {
+            System.out.println("The target matches the user text.");
         } else {
-            System.out.println("The user-defined pattern does not match the beginning of the user string.");
+            System.out.println("The target does not match the user text.");
         }
 
         // Update the user input (treated as a literal string)

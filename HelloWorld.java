2541632b96a9b7ba86d6307c42a9068f8b323c7d
Sheng Hao Liang <gyes51y767p@gmail.com>
2023-10-06 13:27:29 -0700

    modify the constructor and create the new function, so user can get the regex pattern of what they key in

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,32 +1,14 @@
 // HelloWorld.java entry point , class will be in a seperate file
-package api;
-import java.util.Scanner;
+
+
 
 
 public class HelloWorld {
-
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter a target string: ");
-        String userInput = scanner.nextLine();
-        // Create a UserInputRegex object with user-provided string (treated as a literal string)
-        UserInputRegex userInputRegex = new UserInputRegex(userInput);
-        
-        System.out.print("Enter a text: ");
-        String inputText = scanner.nextLine();
-        scanner.close();
-
-        // Check if the pattern matches a user-provided string
-        //String userString = "hello, world!";
-        if (userInputRegex.matches(inputText)) {
-            System.out.println("The target matches the user text.");
-        } else {
-            System.out.println("The target does not match the user text.");
-        }
-
-        // Update the user input (treated as a literal string)
+         UserInputRegex userInputRegex = new UserInputRegex();
         userInputRegex.setUserInput("world");
-        //System.out.println("Updated user input (literal string): " + userInputRegex.getUserInput());
+        System.out.println(userInputRegex.getUserInput());
+        System.out.println(userInputRegex.getUserInputOfRegex());
 
     }
 }
--- UserInputRegex.java
+++ UserInputRegex.java
@@ -1,42 +1,57 @@
-package api;
+// import apiproject;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.regex.PatternSyntaxException;
+import java.util.Scanner;
+
 
 
 
 public class UserInputRegex {
     private String userInput;
+    private String userInputToRegex;
 
     // Constructor that takes user input and creates a regex pattern
-    public UserInputRegex(String userInput) {
-        this.userInput = Pattern.quote(userInput);//this will create the safe regex pattern
+    public UserInputRegex() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a target string: ");
+        this.userInput = scanner.nextLine();
+        this.userInputToRegex = Pattern.quote(userInput);
+        matches();
+        scanner.close();
+
     }
     // Function to check if the regex pattern matches the beginning of the userString
-    public boolean matches(String userString) {
-        try {
+    public void matches() {
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("Enter the text: ");
+            String inputText = scanner.nextLine();
             // Create a Pattern object from the userInput (treated as a regex pattern)
-            Pattern pattern = Pattern.compile(userInput);
-
+            Pattern pattern = Pattern.compile(userInputToRegex);
             // Create a Matcher object to match the pattern against the userString
-            Matcher matcher = pattern.matcher(userString);
-
-            return matcher.find();
-        } catch (PatternSyntaxException e) {//need to fix this
-            System.err.println("Invalid regex pattern syntax: " + e.getDescription());
-            return false;
-        }
+            Matcher matcher = pattern.matcher(inputText);
+            scanner.close();
+            if (matcher.find()) {
+                System.out.println("The target matches the user text.");
+            } else {
+                System.out.println("The target does not match the user text.");
+            }
+    
     }
 
     // Getter method to retrieve the user input (treated as a regex pattern)
     public String getUserInput() {
-        return userInput;
+        return this.userInput;
+    }
+    public String getUserInputOfRegex() {
+        return this.userInputToRegex;
     }
 
     // Setter method to update the user input (treated as a regex pattern)
     public void setUserInput(String userInput) {
-        this.userInput = Pattern.quote(userInput);
+        this.userInput=userInput;
+        this.userInputToRegex = Pattern.quote(userInput);
     }
 
 }
